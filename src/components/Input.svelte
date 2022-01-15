ca8456059a9624c56902d1c7118e3715407ea642
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-01-15 13:39:52 +0700

    Add input component

---
+++ src/components/Input.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { generateCode } from "../utils/generator";
+  // export so it becomes the component props
+  // assign the variable value so it becomes the default value
+  export let word = "";
+
+  // while this one is an internal state data
+  // let sembarang = "hiohoho";
+  $: parsed = generateCode(word);
+</script>
+
+<textarea bind:value={word} rows="3" cols="100" />
+
+<p>Translation:</p>
+<textarea value={parsed} rows="3" cols="100" />
+
+<style>
+  textarea {
+    min-height: 75px;
+    height: 75px;
+    max-height: 75px;
+
+    min-width: 850px;
+    width: 850px;
+    max-width: 850px;
+  }
+</style>
