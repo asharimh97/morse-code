a89c55b9c54cde28695ccc5401ddbaabb466ea48
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-01-16 16:34:57 +0700

    Add better morse translation

--- src/utils/generator.js
+++ src/utils/generator.js
@@ -6,20 +6,59 @@ const mappedCodes = {
   ...symbols
 };
 
+const reversedCodes = Object.entries(mappedCodes).reduce((obj, curr) => {
+  const [key, value] = curr;
+
+  obj[value] = key;
+
+  return obj;
+}, {});
+
+const generateMorse = (input) => {
+  const formattedInput = input
+                          .split("")
+                          .map(alp => {
+                            if (alp) {
+                              return mappedCodes[alp.toUpperCase()] || "#"
+                            }
+
+                            return "";
+                          })
+                          .join("/");
+
+  return formattedInput;
+}
+
+const translateMorse = (input) => {
+  console.log(input);
+  const formattedInput = input
+                          .split("/")
+                          .map(alp => {
+                            if (alp) {
+                              return reversedCodes[alp] || "#"
+                            }
+
+                            return "";
+                          })
+                          .join("");
+
+  return formattedInput;
+}
+
 /**
  * Generate morse code
  * @param {string} input 
  */
 export const generateCode = (input = "") => {
-  // const regex = /^[a-z0-9]$/g
-
+  const regex = /[a-z0-9,\?\\!/\(\)&:;+=_$@]+/g
   // TODO: better converter
   // do regex to check if there's alphabet so we convert from
   // alphabet to morse
 
+  if (input.match(regex))
+    return generateMorse(input);
+
   // otherwise if there's no alphabet/number/punctuations (except space)
   // we should convert from morse to alphabet
-  const formattedInput = input.split("").map(alp => mappedCodes[alp.toUpperCase()]).join("/");
-
-  return formattedInput;
+  return translateMorse(input);
 }
